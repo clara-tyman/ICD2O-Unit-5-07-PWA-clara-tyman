f55b1c04f3a5c4f789a68116c76e09c46eb31455
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-11 00:18:01 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara Tyman All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // define numbers
+  const number = parseInt(document.getElementById("your-number").value)
+  const sum = (number / 2) * (number + 1)
+  // print answer
+  document.getElementById("answer").innerHTML = "The answer is: " + sum
+}
